60578cdf172a077ff3917b1ee4f17e03d3efbaf0
shafinr23 <shafinr23@gmail.com>
2019-12-27 23:21:55 +0600

    add new file

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Dicee 🎲
+
+We’re going to make a Las Vegas dice app. You can make the die roll at the press of a button. With this app in your pocket, you’ll be able to settle any score on the go!
+
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+  void change() {
+    setState(() {
+      rightDiceNum = Random().nextInt(6) + 1;
+      leftDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNum.png'),
+              onPressed: change,
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNum.png'),
+              onPressed: change,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
